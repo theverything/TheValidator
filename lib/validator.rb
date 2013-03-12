6f32354c5725a4afac7c41f82f7eb3d6dfcdb331
theverything <j3ffhorn@gmail.com>
2013-03-11 18:20:38 -0700

    Move validation in to module

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,76 +1,87 @@
-class TheValidator
-  attr_reader :name, :email, :text, :phone
+module TheValidator
 
-  def initialize(attributes = {})
-    @name = attributes[:name] || ""
-    @email = attributes[:email] || ""
-    @text = attributes[:text] || ""
-    @phone = attributes[:phone] || ""
-    @errors = {}
-  end
+  class Validate
+    attr_reader :name, :email, :text, :phone, :errors
 
-  def validate_all
-    validation_passed = true
+    def initialize(attributes = {})
+      @name = attributes[:name] || ""
+      @email = attributes[:email] || ""
+      @text = attributes[:text] || ""
+      @phone = attributes[:phone] || ""
+      @errors = []
+    end
 
-    validations = ['validate_name?', 'validate_email?', 'validate_text?', 'validate_phone?']
-    validations.each do |validation_method_name|
-      if !self.send(validation_method_name.to_sym)
-        validation_passed = false
+    def valid_all?
+      validation_passed = true
+
+      validation_methods = ['valid_name?', 'valid_email?', 'valid_text?', 'valid_phone?']
+      validation_methods.each do |validation_method_name|
+        if !self.send(validation_method_name.to_sym)
+          validation_passed = false
+        end
+      end
+
+      if validation_passed
+        return true
+      else
+        return false
       end
     end
 
-    if validation_passed
-      return { valid: true, errors: @errors }
-    else
-      return { valid: false, errors: @errors }
+    def valid_name?
+      if @name.empty?
+        @errors << "Name is empty"
+        return false
+      elsif @name.match(/\A[a-z ,.'-]+\z/i)
+        return true
+      else
+        @errors << "Name is not valid"
+        return false
+      end
     end
+
+    def valid_email?
+      if @email.empty?
+        @errors << "Email is empty"
+        return false
+      elsif @email.match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+        return true
+      else
+        @errors << "Email is not valid"
+        return false
+      end
+    end
+
+    def valid_text?
+      if @text.empty?
+        @errors << "Text is empty"
+        return false
+      else
+        return true
+      end
+    end
+
+    def valid_phone?
+      if @phone.empty?
+        @errors << "Phone number is empty"
+        return false
+      elsif @phone.match(/\A\+?\d?-?\(?\d{3}\)?-?\d{3}-?\d{4}\z/)
+        return true
+      else
+        @errors << "Phone number is not valid"
+        return false
+      end
+    end
+
   end
 
-  def validate_name?
-    if @name.empty?
-      @errors[:name_err] = "Name is empty"
-      return false
-    elsif @name.match(/\A[a-z ,.'-]+\z/i)
-      return true
-    else
-      @errors[:name_err] = "Name is not valid"
-      return false
-    end
-  end
-
-  def validate_email?
-    if @email.empty?
-      @errors[:email_err] = "Email is empty"
-      return false
-    elsif @email.match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
-      return true
-    else
-      @errors[:email_err] = "Email is not valid"
-      return false
-    end
-  end
-
-  def validate_text?
-    if @text.empty?
-      @errors[:text_err] = "Text is empty"
-      return false
-    else
-      return true
-    end
-  end
-
-  def validate_phone?
-    if @phone.empty?
-      @errors[:phone_err] = "Phone number is empty"
-      return false
-    elsif @phone.match(/\A\+?\d?-?\(?\d{3}\)?-?\d{3}-?\d{4}\z/)
-      return true
-    else
-      @errors[:phone_err] = "Phone number is not valid"
-      return false
-    end
-  end
-
-  
 end
+
+
+
+
+
+
+
+
 
